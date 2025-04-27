71c66ed856026f157c888e5f30bba7b9f06df439
Thrshan <thrshan.jeevaraj@gmail.com>
2025-04-27 23:14:07 +0530

    Small update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css'
@@ -8,7 +8,7 @@ import UnderConstruction from './pages/UnderConstruction.tsx'
 import MarriagePage from './pages/MarriagePage.tsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <Router>
