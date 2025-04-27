37b40c91fc460a2fb464f9279824acc04a85681a
Thrshan <thrshan.jeevaraj@gmail.com>
2025-04-27 09:28:10 +0530

    add pages

---
+++ src/pages/MarriagePage.tsx
@@ -0,0 +1,10 @@
+import './MarriagePage.css'
+function MarriagePage() {
+    return (
+        <>
+            <h1 className='heading'>Marriage</h1>
+        </>
+    )
+}
+
+export default MarriagePage
