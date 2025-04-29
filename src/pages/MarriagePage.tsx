fa2489ac9c91893965fb60b826900e94358d9c66
Thrshan <thrshan.jeevaraj@gmail.com>
2025-04-30 00:55:49 +0530

    Some change

--- src/pages/MarriagePage.tsx
+++ src/pages/MarriagePage.tsx
@@ -3,7 +3,7 @@ function MarriagePage() {
     return (
         <>
         <div>
-            <h1 className='heading'>Marriage</h1>
+            <h1 className='heading'>Marrriage</h1>
             <h2>Christian marriage</h2>
             <p>8th June 2025</p>
             <p>Jeevan Jothi Church, Chennai - 116</p>
